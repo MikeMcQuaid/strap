bd592e949c547925dcecd5b1ca5a756dbb2a28b3
Mike McQuaid <mike@mikemcquaid.com>
2018-08-05 12:36:23 +0100

    web/app: RuboCop and HTML style cleanup.

--- web/app.rb
+++ web/app.rb
@@ -7,8 +7,8 @@ require "awesome_print" if ENV["RACK_ENV"] == "development"
 GITHUB_KEY = ENV["GITHUB_KEY"]
 GITHUB_SECRET = ENV["GITHUB_SECRET"]
 SESSION_SECRET = ENV["SESSION_SECRET"] || SecureRandom.hex
-STRAP_ISSUES_URL = ENV["STRAP_ISSUES_URL"] || \
-                   "https://github.com/MikeMcQuaid/strap/issues/new"
+STRAP_ISSUES_URL_DEFAULT = "https://github.com/MikeMcQuaid/strap/issues/new"
+STRAP_ISSUES_URL = ENV["STRAP_ISSUES_URL"] || STRAP_ISSUES_URL_DEFAULT
 STRAP_BEFORE_INSTALL = ENV["STRAP_BEFORE_INSTALL"]
 CUSTOM_HOMEBREW_TAP = ENV["CUSTOM_HOMEBREW_TAP"]
 CUSTOM_BREW_COMMAND = ENV["CUSTOM_BREW_COMMAND"]
@@ -24,8 +24,8 @@ end
 get "/auth/github/callback" do
   auth = request.env["omniauth.auth"]
   session[:auth] = {
-    'info' => auth["info"],
-    'credentials' => auth["credentials"]
+    "info"        => auth["info"],
+    "credentials" => auth["credentials"],
   }
 
   return_to = session.delete :return_to
@@ -44,17 +44,45 @@ get "/" do
   end
 
   @title = "👢 Strap"
-  @text = <<~EOS
+  @text = <<~HTML
     To Strap your system:
     <ol>
       #{before_install_list_item}
-      <li><a href="/strap.sh"><button type="button" class="btn btn-outline-primary btn-sm">Download the <code>strap.sh</code></button></a> that's been customised for your GitHub user (or <a href="/strap.sh?text=1">view it</a> first). This will prompt for access to your email, public and private repositories; you'll need to provide access to any organizations whose repositories you need to be able to <code>git clone</code>. This is used to add a GitHub access token to the <code>strap.sh</code> script and is not otherwise used by this web application or stored anywhere.</li>
-      <li>Run Strap in Terminal.app with <code>bash ~/Downloads/strap.sh</code>.</li>
-      <li>If something failed, run Strap with more debugging output in Terminal.app with <code>bash ~/Downloads/strap.sh --debug</code> and file an issue at <a href="#{STRAP_ISSUES_URL}">#{STRAP_ISSUES_URL}</a></li>
-      <li>Delete the customised <code>strap.sh</code></a> (it has a GitHub token in it) in Terminal.app with <code>rm -f ~/Downloads/strap.sh</code></a></li>
-      <li>Install additional software with <code>brew install</code> and <code>brew cask install</code>.</li>
+      <li>
+        <a href="/strap.sh">
+          <button type="button" class="btn btn-outline-primary btn-sm">
+            Download the <code>strap.sh</code>
+          </button>
+        </a>
+        that's been customised for your GitHub user (or
+        <a href="/strap.sh?text=1">view it</a>
+        first). This will prompt for access to your email, public and private
+        repositories; you'll need to provide access to any organizations whose
+        repositories you need to be able to <code>git clone</code>. This is
+        used to add a GitHub access token to the <code>strap.sh</code> script
+        and is not otherwise used by this web application or stored
+        anywhere.
+      </li>
+      <li>
+        Run Strap in Terminal.app with <code>bash ~/Downloads/strap.sh</code>.
+      </li>
+      <li>
+        If something failed, run Strap with more debugging output in
+        Terminal.app with <code>bash ~/Downloads/strap.sh --debug</code> and
+        file an issue at <a href="#{STRAP_ISSUES_URL}">#{STRAP_ISSUES_URL}</a>
+      </li>
+      <li>
+        Delete the customised <code>strap.sh</code> (it has a GitHub token
+        in it) in Terminal.app with
+        <code>rm -f ~/Downloads/strap.sh</code>
+      </li>
+      <li>
+        Install additional software with
+        <code>brew install</code> and
+        <code>brew cask install</code>.
+      </li>
     </ol>
-  EOS
+  HTML
   erb :root
 end
 
